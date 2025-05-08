653dc9c698ca554a5c30b3b6b3bbc6f5db4dd1dd
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-05-08 11:47:50 +0700

    membuat fungsi untuk menghapus node

--- anjayani.cpp
+++ anjayani.cpp
@@ -62,6 +62,18 @@ class SingelLinkedList {    //membuat class SingelLinkedList
             }
             return (*current != NULL);
         }
+        bool delNode(int nim) {     //membuat fungsi untuk menghapus node
+            node *current, *previous;
+            if (!Search(nim, &previous, &current))
+                return false;
+            if (current == START)
+                START = START->next;
+            else
+                previous->next = current->next;
+            delete current;
+            return true;
+        }
+        
         
     };
 
