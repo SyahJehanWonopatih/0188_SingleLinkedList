435f5735bb22dfa85d5b14b7e6072cff151abac6
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-05-08 11:43:56 +0700

    membuat fungsi untuk menambah node

--- anjayani.cpp
+++ anjayani.cpp
@@ -16,9 +16,42 @@ class SingelLinkedList {    //membuat class SingelLinkedList
             START = NULL;
         }
 
+        void addNode() {     //membuat fungsi untuk menambah node
+            int nim;
+            cout << "\nMasukkan Nomor Mahasiswa : ";
+            cin >> nim;
+
+            node *nodeBaru = new node;
+            nodeBaru -> noMhs = nim;
+
+            if(START == NULL || nim <= START->noMhs) {  
+                if ((START != NULL) && (nim == START->noMhs)) {
+                    cout << "\nDuplikasi no mahasiswa tidak di izinkan\n";
+                    return;
+                }
+                nodeBaru->next = START;
+                START = nodeBaru;
+                return;
+            }
+            node *previous = START;
+            node *current = START;
+            while ((current != NULL) && (current->noMhs < nim)) {
+                  if (nim == current->noMhs) {
+                    cout << "\nDuplikasi noMhs tidak di izinkan\n";
+                    return;
+                }
+                previous = current;
+                current = current->next;
+            }
+
+            nodeBaru->next = current;
+            previous->next = nodeBaru;
+
+        }
         
+        }
     
-};
+
 
 
 
